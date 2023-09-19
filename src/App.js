5e487f430139a2bde7eabe9045ae8707c20d7f8c
Sebastian Marrera <smarrera@cincinnatus.edu.do>
2023-09-19 00:17:43 -0400

    Finished Styled Components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import Container from "./Components/Container";
 import Header from "./Components/Header";
+import GlobalStyle from "./GlobalStyle";
+
+import { ThemeProvider } from "styled-components";
+import { temaClaro, temaOscuro } from "./Components/UI/temas";
+import { BtnTema } from "./Components/UI";
+import SwitcherTema from "./Components/SwitcherTema";
 
 function App() {
+  const [tema, setTema] = useState(true)
+
+  const ToggleTema = () => {
+    setTema((tema) => !tema)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={tema ? temaClaro : temaOscuro}>
+      <GlobalStyle />
+      <BtnTema onClick={ToggleTema}>
+        <SwitcherTema tema={tema}/>
+      </BtnTema>
       <Header />
       <Container />
-    </>
+    </ThemeProvider>
   );
 }
 
